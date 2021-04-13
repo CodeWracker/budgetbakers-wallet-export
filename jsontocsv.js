c2c02adbe92c461941ce197265a48a0d95b2d81a
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-04-13 05:54:38 -0300

    corrigi um erro

--- jsontocsv.js
+++ jsontocsv.js
@@ -10,7 +10,7 @@ console.log = function (d) {
   log_file.write(util.format(d) + "\n");
   log_stdout.write(util.format(d) + "\n");
 };
-
+console.log("Category,Account,Value,Description,Date");
 for (let i = 0; i < data.length; i++) {
   console.log(data[i].Categoria + "," + data[i].Conta + "," + data[i].Valor + "," + data[i].Descricao + "," + data[i].Data);
 }
