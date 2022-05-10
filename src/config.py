3ab521a37a5dcbf35adc3e29df37a88d110d02da
AmadeusG <45653884+AmadeusG@users.noreply.github.com>
2022-05-10 14:10:02 +0800

    Change name of HTML files
    HTML path and its name should as same as "Files Structure" shows.

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 import os
 
 FILE_PATH = os.getcwd() + "//../site/"
-# PUT THE HTML FILE NAME HERE, I USE "REGISTROS-WALLET-P" BY DEFAULT
-FILE_PATH = FILE_PATH + "registros-wallet-p.html"
+# PUT THE HTML FILE NAME HERE, I USE "REGISTROS-WALLET" BY DEFAULT
+FILE_PATH = FILE_PATH + "registros-wallet.html"
 
 def configuration():
     
